a8ef3207f25d1d845336d795eed4d44dfc0bbfe5
sofia_liggio <sofialiggio994@gmail.com>
2025-08-01 11:47:55 +0200

    fetch for json

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+let allAds = [];
+
 document.addEventListener("DOMContentLoaded", () => {
   const selectCategorie = document.getElementById("select-categorie");
   const selectCitta = document.getElementById("select-citta");
   const selectOrdina = document.getElementById("select-order");
+
+  // Funzione per popolare un menu a tendina
+  function populateSelect(selectElement, data, placeholder) {
+    selectElement.innerHtml = "";
+    const defaultOption = document.createElement("option");
+    defaultOption.textContent = placeholder;
+    defaultOption.value = "";
+    defaultOption.selected = true;
+    defaultOption.disabled = true;
+    selectElement.appendChild(defaultOption);
+    data.sort();
+    data.forEach((item) => {
+      const option = document.createElement("option");
+      option.value = item;
+      option.textContent = item;
+      selectElement.appendChild(option);
+    });
+  }
 
   // Caricamento e popolamento delle categorie
   fetch("./data/categories.json").then((response) => {
@@ -36,5 +56,22 @@ document.addEventListener("DOMContentLoaded", () => {
       .catch((error) => {
         console.error("Errore ne lcaricamento delle città:", error);
       });
+
+    // Caricamento e salvataggio degli annunci
+    fetch("./data/sales_dataset.json")
+      .then((response) => response.json())
+      .then((data) => {
+        allAds = data;
+        console.log("Annunci caricati:", allAds);
+      })
+      .catch((error) => {
+        console.error("Errore nel caricamento degli annuni:", error);
+      });
+
+    // EventListener per il menu di ordinamento
+    selectOrdina.addEventListener("change", (event) => {
+      const opzioneSelezionata = event.target.value;
+      console.log("Ordinamento selezionato:", opzioneSelezionata);
+    });
   });
 });
